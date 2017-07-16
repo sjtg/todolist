b83df21a810f4f4a3bc3ac59a192253ffded6dc5
sjtg <chipilej@gmail.com>
2017-07-16 14:35:21 +0200

    1:changed databases from sql lite3(default) to mysql 2: added a new database name , user and password 3: created class for todo in models.py the user form

--- employees/employees/settings.py
+++ employees/employees/settings.py
@@ -60,14 +60,14 @@ WSGI_APPLICATION = 'employees.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.mysql',
-        # 'NAME': 'todolist',
-        # 'USER' : 'root',
-        # 'PASSWORD' : 'jado2016asd',
-        #'HOST': '',
-        # 'PORT' : '',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'todolist',
+         'USER' : 'root',
+         'PASSWORD' : 'jado2016asd',
+        'HOST': '',
+         'PORT' : '',
     }
 }
 
